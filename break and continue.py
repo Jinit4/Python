52e3f6ab788b1bb495cf958b79affe726ea7a5e8
Jinit4 <JD24795N@pace.edu>
2023-03-31 03:32:18 -0400

    Break and Continue

---
+++ break and continue.py
@@ -0,0 +1,11 @@
+Students =["Jinit","Jimmy","Jim","Jerry","Jini"]
+
+for student in Students :
+    if student == "Jim":
+        break; #here we use break whenever we want to get the output to acertain extent from that point after it will come out of the loop
+    print (student)
+Greeting =["Namaste","Hello", "Hey", "Hi", "Bonjour"]
+for greets in Greeting:
+    if greets == "Hi":
+        continue; #continue is used to skip a certain thing whenever we want in a list and then the list is printed
+    print(greets)
