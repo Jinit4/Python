181337c2262a5e18f5c8389876dafac05bb71060
Jinit4 <JD24795N@pace.edu>
2023-03-31 02:10:13 -0400

    Project 1

---
+++ If - Else Statements.py
@@ -0,0 +1,9 @@
+#If - Else Statements elif 
+age = 13 
+if age>=18:
+    print("You are an adult")
+    print("You can Vote")
+elif age <= 18 and age >3:
+    print ("You are a child")
+else :
+    print("You are not elligile to enter")
